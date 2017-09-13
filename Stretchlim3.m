66962964cdd79293df698b41b508f8600be4688e
XiaotongLu <31257663+XiaotongLu@users.noreply.github.com>
2017-09-13 17:37:31 -0500

    Update Stretchlim3.m

--- Stretchlim3.m
+++ Stretchlim3.m
@@ -8,3 +8,10 @@ else
     error('must have one or two inputs');
 end
 end
+
+
+function J= Stretchlim(I)
+J=imadjust(I,stretchlim(I),[0.01 0.99]);
+
+function J2=Stretchlim2(I,x)
+J2=imadjust(I,stretchlim(I),[x 1-x]);
